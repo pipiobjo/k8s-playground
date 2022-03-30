58ba17c915f301ad1464523c9236e88dfe58174f
Björn Pipiorke <bjoern.pipiorke@prodyna.com>
2022-03-30 12:43:12 +0200

    init

--- scripts/internal/k8s-env.sh
+++ scripts/internal/k8s-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DOCKER_INTERNAL_REGISTRY_NAME='myscope--kind-registry'
+DOCKER_INTERNAL_REGISTRY_NAME='myscope-kind-registry'
 DOCKER_REGISTRY_PORT='5000'
 KIND_CLUSTER_NAME='myscope' # only lower case allowed and has to be a valid RFC-1123 DNS subdomain
